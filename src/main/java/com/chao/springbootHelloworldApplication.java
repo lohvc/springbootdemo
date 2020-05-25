454b02480183e7ae58d08871313b5e6d0e7a695e
lohvc <453111462@qq.com>
2020-05-25 09:26:55 +0800

    项目初始化完成

---
+++ src/main/java/com/chao/springbootHelloworldApplication.java
@@ -0,0 +1,18 @@
+package com.chao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * \* User: lohvc
+ * \* Date: 2020/5/25
+ * \* Time: 9:23
+ * \
+ */
+@SpringBootApplication
+public class springbootHelloworldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(springbootHelloworldApplication.class,args);
+    }
+}
